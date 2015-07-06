1486c1fae098a0c708e6f29967a9d9300c33ba3c
Zephyr Hansen <oozefzefoo@gmail.com>
2015-07-05 19:35:57 -0500

    added LetterToYourself.java

---
+++ LetterToYourself.java
@@ -0,0 +1,15 @@
+public class LetterToYourself
+{
+  public static void main ( String[] args )
+  {
+    System.out.println( "+--------------------------------------------------------------+" );
+    System.out.println( "|                                                              |" );
+    System.out.println( "|                                NOT Bill Gates                |" );
+    System.out.println( "|                                2 Microfost Ways              |" );
+    System.out.println( "|                                likkee nu windows             |" );
+    System.out.println( "|                                only doors cause ye           |" );
+    System.out.println( "|                                Bluemond, MI 12345            |" );
+    System.out.println( "|                                                              |" );
+    System.out.println( "+--------------------------------------------------------------+" );
+  }
+}
